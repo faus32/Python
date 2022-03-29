61f5a7ea61d6d70f73f62156932f63046fe28f34
Faus32 <faus32@protonmail.com>
2022-03-29 22:47:48 +0200

    Terminado el reto4 #4

--- reto python/reto4/utils.py
+++ reto python/reto4/utils.py
@@ -8,7 +8,7 @@ import os
 
 # listado de imagenes
 
-def listar_imagenes (ruta_imagenes:Path()):
+def listar_imagenes (ruta_imagenes):
     for archivo in[x for x in ruta_imagenes.iterdir() if not x.is_dir()]:
         if not archivo.is_dir() and mimetypes.guess_type(archivo)[0]=='image/jpeg':
             print (archivo)
